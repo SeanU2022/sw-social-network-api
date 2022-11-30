4ce732fa9ed96132a7a9a40bf1813df1ac5a2613
Sean Wallace <sean.wallace.australia@gmail.com>
2022-11-30 19:46:00 +1100

    code testing Heroku/Mongo deploy

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const routes = require('./routes');
 
 const cwd = process.cwd();
 
+// note: must be uppercase PORT to deploy
+// lowercase .port is ok on the localhost
 const PORT = process.env.PORT || 3001;
 const app = express();
 
